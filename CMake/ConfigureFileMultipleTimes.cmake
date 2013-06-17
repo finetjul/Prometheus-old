a95ef0a63b4d8ca9b4c936cfe1bc59aa82b32e85
Johan Andruejol <johan.andruejol@kitware.com>
2013-06-17 15:23:55 -0400

    TO Squash with first commit

--- CMake/ConfigureFileMultipleTimes.cmake
+++ CMake/ConfigureFileMultipleTimes.cmake
@@ -1,13 +1,13 @@
 # Usage:
 # \code
-# configure_file_multiple INPUT_FILE OUTPUT_FILE NUMBER_OF_CONFIGURE)
+# configure_file_multiple_times INPUT_FILE OUTPUT_FILE NUMBER_OF_CONFIGURE)
 # \endcode
 #
 # INPUT_FILE and OUTPUT_FILE are used to configure file using the configure_file.
 # NUMBER_OF_CONFIGURE is the number of times the file is configured.
 #
 
-macro(configure_file_multiple)
+macro(configure_file_multiple_times)
   include(CMakeParseArguments)
   set(oneValueArgs INPUT_FILE OUTPUT_FILE NUMBER_OF_CONFIGURE)
   cmake_parse_arguments(MY "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
